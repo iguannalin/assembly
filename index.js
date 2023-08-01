50192961b02228c0369fe105cdbe09b5f22d8bc4
seasons986 <annalin12345@gmail.com>
2023-08-01 16:14:11 -0400

    hello

--- index.js
+++ index.js
@@ -4,16 +4,25 @@ window.addEventListener("load", () => {
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
   }
+
+  const content = document.getElementById("content");
   
   function getLines(t) {
-    const div = document.createElement("div");
-    const length = 10;
-    const index = getRandomInt(length, t.length - length);
-    div.innerHTML = t.substring(index-length, index);
-    document.body.appendChild(div);
+    for (let i = 0; i < 15; i++) {
+      const p = document.createElement("p");
+      const length = 10;
+      const index = getRandomInt(length, t.length - length);
+      p.innerHTML = t.substring(index-length, index);
+      p.style.paddingTop = `${getRandomInt(0,27)}px`;
+      p.style.paddingLeft = `${getRandomInt(0,5)}px`;
+      p.style.paddingRight = `${getRandomInt(0,5)}px`;
+      content.appendChild(p);
+      console.log(p.innerText);
+    }
   }
 
   fetch("constitution.txt").then((file) => file.text()).then((t) => {
-    for (let i = 0; i < 10; i++) getLines(t);
+    content.innerHTML = "";
+    getLines(t);
   })
 });
